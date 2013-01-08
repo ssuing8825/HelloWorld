cc36b1a591170d339fb53319b4575e48c9cb2692
Steven Suing <Steven.suing@gmail.com>
2013-01-08 11:04:55 -0500

    added a client to send more messages

---
+++ HelloWorldConsoleClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+using log4net;
+using Messages;
+
+namespace HelloWorldConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessage();
+        }
+
+        static void SendMessage()
+        {
+            var Bus = Configure.With()
+                     .DefaultBuilder()
+                     .XmlSerializer()
+                     .MsmqTransport()
+                       .IsTransactional(false)
+                       .PurgeOnStartup(false)
+                     .UnicastBus()
+                       .ImpersonateSender(false)
+                     .CreateBus()
+                     .Start();
+
+            var message = new Request { SaySomething = "Say something again fro a console application" };
+            Bus.Send(message);
+            LogManager.GetLogger("MessageSender").Fatal("Sent message.");
+
+
+
+        }
+    }
+}
